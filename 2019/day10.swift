94c913dfe15c240e1991660e42eae588a76f0629
Duncan Stevenson-Price <duncan@homegrownsw.com>
2019-12-10 20:49:32 +0000

    Day 10 Part 1 Solution

--- 2019/day10.swift
+++ 2019/day10.swift
@@ -4,7 +4,7 @@ import Foundation
 // Inputs & Setup
 // **************
 
-let inputURL: URL = URL(fileURLWithPath: "input/day10test.txt")
+let inputURL: URL = URL(fileURLWithPath: "input/day10.txt")
 var input: String = ""
 if let loadedFile = try? String(contentsOf: inputURL, encoding: .utf8) {
 	input = loadedFile
@@ -68,7 +68,7 @@ for station in asteroidLocations {
 		var isReachable: Int = 1
 		
 		if asteroidx == stationx {
-			// Asteroid and Station are horizontally aligned
+			// Asteroid and Station are vertically aligned
 			for blocker in asteroidLocations {
 				if blocker == asteroid || blocker == station { continue }
 				let blockerx: Int! = blocker["x"]
@@ -83,7 +83,7 @@ for station in asteroidLocations {
 			}
 		}
 		else if asteroidy == stationy {
-			// Asteroid and Station are veritcally aligned
+			// Asteroid and Station are horizontally aligned
 			for blocker in asteroidLocations {
 				if blocker == asteroid || blocker == station { continue }
 				let blockerx: Int! = blocker["x"]
@@ -98,7 +98,7 @@ for station in asteroidLocations {
 			}
 		}
 		else {
-			let slope = Float((asteroidy-stationy)/(asteroidx-stationx))
+			let slope = (Float(asteroidy-stationy)/Float(asteroidx-stationx))
 			for blocker in asteroidLocations {
 				if blocker == asteroid || blocker == station { continue }
 				let blockerx: Int! = blocker["x"]
@@ -108,12 +108,8 @@ for station in asteroidLocations {
 				   (stationx > blockerx && blockerx > asteroidx && stationy < blockery && blockery < asteroidy) ||
 				   (stationx > blockerx && blockerx > asteroidx && stationy > blockery && blockery > asteroidy) {
 					// Potential Blocker (In range)
-					print(String(format:"\nBlocker: %@", blocker))
-					print(String(format:"Station: %@", station))
-					print(String(format:"Asteroid: %@", asteroid))
-					print("Potential Blocker\n")
 					let lhs = Float(blockery - stationy)
-					let rhs = Float(slope * Float(blockery - stationy))
+					let rhs = Float(slope * Float(blockerx - stationx))
 					if lhs == rhs {
 						// Asteroid is not reachable from station
 						isReachable = 0
@@ -128,4 +124,4 @@ for station in asteroidLocations {
 }
 //print(counts)
 let sortedCounts = counts.sorted(by: >)
-print(sortedCounts[0])
+print(String(format:"The maximum number of asteroid is: %d"),sortedCounts[0])
