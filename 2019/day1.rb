8c219153ca3cace9c41a929d40154042859d98e5
Duncan Stevenson-Price <duncan@homegrownsw.com>
2020-11-22 15:07:13 +0000

    Re-wrote 2019/1 in Ruby for a wee taste of Ruby.

---
+++ 2019/day1.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+
+=begin
+I decided to re-do Day 1 in Ruby as I've never used
+Ruby before and wanted to get a wee taste of it.
+
+I may use Ruby for AoC 2020!
+=end
+
+
+arrayOfModuleMasses = File.read("input/day1.txt").split
+
+if arrayOfModuleMasses.length != nil
+	puts "File loaded successfully!"
+	
+	puts "Part 1"
+	
+	fuelRequired = 0
+	
+	arrayOfModuleMasses.each do |moduleMass|
+		fuelforModule = (moduleMass.to_f/3).floor - 2
+		fuelRequired += fuelforModule
+	end
+	
+	puts "Total fuel required: #{fuelRequired}"
+	
+	puts "Part 2"
+	
+	fuelRequired = 0
+	
+	arrayOfModuleMasses.each do |moduleMassString|
+		moduleMass = moduleMassString.to_f
+		
+		while moduleMass > 0 do
+			fuelforModule = (moduleMass.to_f/3).floor - 2
+			if fuelforModule > 0
+				fuelRequired += fuelforModule
+			end
+			moduleMass = fuelforModule
+		end
+	end
+	
+	puts "Total fuel required: #{fuelRequired}"
+	
+end
