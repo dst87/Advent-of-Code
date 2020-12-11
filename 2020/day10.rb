648baa0be4cfa136be6543042b4571084bb57988
Duncan Stevenson-Price <duncan@psmail.co>
2020-12-11 18:18:03 +0000

    AoC 2020 Day 10 Part 2 (With math borrowed from someone else).

--- 2020/day10.rb
+++ 2020/day10.rb
@@ -31,3 +31,22 @@ puts "The answer to part 1 is #{one_jolt_count * thr_jolt_count}."
 
 puts "\nPart 2".red.on_black.underline
 
+# I had no idea how to do this, so my part 2 solution is an implementation
+# of a solution I found on Reddit here:
+# https://www.reddit.com/r/adventofcode/comments/ka8z8x/2020_day_10_solutions/gf9pg9n
+# It's very elegant, and I can work out how and why it works, but I'd never
+# have arrived at the solution myself.
+
+number_of_links = Hash.new
+number_of_links[0] = 1
+
+ratings.each do |rating|
+	next if rating == 0
+	a = number_of_links[rating-1] || 0
+	b = number_of_links[rating-2] || 0
+	c = number_of_links[rating-3] || 0
+	
+	number_of_links[rating] = a + b + c
+end
+
+puts "There are #{number_of_links[ratings.last]} possible ways of connecting my device to the outlet. 🔌📱"
