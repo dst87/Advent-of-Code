aceb710fd59cf53fbc13f7e994d1bcc6243e7421
Duncan Stevenson-Price <duncan@psmail.co>
2021-12-06 08:00:55 +0000

    2021 Day 6 Part 2

--- 2021/day6.rb
+++ 2021/day6.rb
@@ -4,6 +4,18 @@ require 'colorize'
 
 fish = File.read("input/day6.txt").split(",").map!(&:to_i)
 
+fishCount = []
+
+fishCount[0] = fish.count(0)
+fishCount[1] = fish.count(1)
+fishCount[2] = fish.count(2)
+fishCount[3] = fish.count(3)
+fishCount[4] = fish.count(4)
+fishCount[5] = fish.count(5)
+fishCount[6] = fish.count(6)
+fishCount[7] = fish.count(7)
+fishCount[8] = fish.count(8)
+
 puts "\nPart 1".red.on_black.underline
 
 def advance(fish)
@@ -28,5 +40,21 @@ end
 
 puts "After #{days} days there will be #{fish.length} fish."
 		
+		
 puts "\nPart 2".red.on_black.underline
 
+days = 256
+
+def advanceCounts(counts)
+	newCounts = counts
+	newFish = newCounts.shift
+	newCounts[6] += newFish
+	newCounts.push newFish
+	return newCounts
+end
+
+for i in 1..days
+	fishCount = advanceCounts(fishCount)
+end
+
+puts "After #{days} days there will be #{fishCount.sum} fish."
